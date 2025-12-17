f684086b4b01dd58e4de784b41857d7354fd1120
Default Name <letsdevapps@gmail.com>
2025-12-17 14:20:13 -0300

    encapsular rotas de endpoints no metodo setuproutes

--- src/main.go
+++ src/main.go
@@ -7,11 +7,21 @@ import (
 	"net/http"
 )
 
+func main() {
+	setUpRoutes()
+
+	log.Println("Servidor rodando na porta 8080...")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatalf("Erro ao iniciar o servidor: %v", err)
+	}
+}
+
 func index(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "----- Go Rest | Main -----")
 }
 
-func main() {
+func setUpRoutes() {
 	http.HandleFunc("/api/old", index)
 	//http.ListenAndServe(":8080", nil)
 
@@ -22,10 +32,4 @@ func main() {
 	http.HandleFunc("/api/boolean", handlers.BooleanHandler2)
 	http.HandleFunc("/api/user", handlers.GetUser)
 	http.HandleFunc("/api/users", handlers.GetUsers)
-
-	log.Println("Servidor rodando na porta 8080...")
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		log.Fatalf("Erro ao iniciar o servidor: %v", err)
-	}
 }
